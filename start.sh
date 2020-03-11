3da900b24490a2cf5c55c9ea2a760d15efd87cca
Noa <topoyr@gmail.com>
2020-03-11 15:07:54 +0700

    chore: add spec and scripts

---
+++ start.sh
@@ -0,0 +1,11 @@
+export PORT=4000
+export MIX_ENV=dev
+export DATABASE_URL=postgresql://postgres:@localhost:5432/explorer?ssl=false
+export CHAIN_SPEC_PATH=./docker/spec.json
+export BLOCKSCOUT_PROTOCOL=http
+export BLOCKSCOUT_HOST=0.0.0.0
+export ETHEREUM_JSONRPC_VARIANT=parity
+export ETHEREUM_JSONRPC_HTTP_URL=http://138.68.71.224:8545
+export ETHEREUM_JSONRPC_WS_URL=ws://138.68.71.224:8546
+export ETHEREUM_JSONRPC_TRACE_URL=http://138.68.71.224:8545 
+mix phx.server
