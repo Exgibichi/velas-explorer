ebe051465998c86eb64ce55f8b22e026a347e4e8
Noa <topoyr@gmail.com>
2020-05-16 14:21:35 +0700

    add pull image to upgrade.sh

--- docker/upgrade.sh
+++ docker/upgrade.sh
@@ -3,5 +3,7 @@ pm2 stop explorer
 docker kill $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker volume rm $(docker volume ls -q)
+docker rmi $(docker images topoyr/velas-explorer-prod -q)
+docker pull topoyr/velas-explorer-prod
 make postgres
 pm2 start explorer
