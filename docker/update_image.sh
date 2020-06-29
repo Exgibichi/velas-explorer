898b0eb6524ca33c56396b05029c98d3d06525fa
noa <topoyr@gmail.com>
2020-06-29 20:02:35 +0700

    chore: change scripts for testnet

--- docker/update_image.sh
+++ docker/update_image.sh
@@ -1,4 +1,4 @@
 docker kill $(docker ps -a -q)
 docker rm $(docker ps -a -q)
-docker rmi $(docker images topoyr/velas-explorer-prod -q)
-docker pull topoyr/velas-explorer-prod
+docker rmi $(docker images topoyr/velas-explorer-test -q)
+docker pull topoyr/velas-explorer-test
--- docker/upgrade.sh
+++ docker/upgrade.sh
@@ -2,7 +2,7 @@ git pull
 pm2 stop explorer
 docker kill $(docker ps -a -q)
 docker rm $(docker ps -a -q)
-docker rmi $(docker images topoyr/velas-explorer-prod -q)
-docker pull topoyr/velas-explorer-prod
+docker rmi $(docker images topoyr/velas-explorer-test -q)
+docker pull topoyr/velas-explorer-test
 make postgres
 pm2 start explorer
